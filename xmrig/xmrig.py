f2d403c89bdd8af71c3eee08a1fcd2d142404e3b
CoulterStutz <coultercash@gmail.com>
2024-06-26 15:44:03 -0600

    [XMRIG]: Return accepted and rejected jobs

--- xmrig/xmrig.py
+++ xmrig/xmrig.py
@@ -1,52 +1,40 @@
+import requests
+
+
 class XMRig:
     def __init__(self, ip, port, access_token: str = None):
         self._ip = ip
         self._port = port
         self._access_token = access_token
-
         self._base_url = f"http://{ip}:{port}/2"
         self._summary_url = f"{self._base_url}/summary"
-        self._stats_url = f"{self._base_url}/stats"
-        self._config_url = f"{self._base_url}/config"
-        self._version_url = f"{self._base_url}/version"
-        self._log_url = f"{self._base_url}/log"
-        self._backends_url = f"{self._base_url}/backends"
-        self._workers_url = f"{self._base_url}/workers"
-        self._results_url = f"{self._base_url}/results"
-        self._connections_url = f"{self._base_url}/connections"
-        self._threads_url = f"{self._base_url}/threads"
+        self._hashrate = None
+        self._uptime = None
+        self._accepted_jobs = None
+        self._rejected_jobs = None
+        self._paused = None
 
-    def _format_summary(self, summary):
-        formatted_summary = {
-            "ID": summary.get("id"),
-            "Worker ID": summary.get("worker_id"),
-            "Uptime (seconds)": summary.get("uptime"),
-            "Restricted": summary.get("restricted"),
-            "Memory": summary.get("resources", {}).get("memory", {}),
-            "Load Average": summary.get("resources", {}).get("load_average"),
-            "Hardware Concurrency": summary.get("resources", {}).get("hardware_concurrency"),
-            "Features": summary.get("features"),
-            "Results": summary.get("results"),
-            "Algorithm": summary.get("algo"),
-            "Connection": summary.get("connection"),
-            "Version": summary.get("version"),
-            "Kind": summary.get("kind"),
-            "User Agent": summary.get("ua"),
-            "CPU": summary.get("cpu"),
-            "Donate Level": summary.get("donate_level"),
-            "Paused": summary.get("paused"),
-            "Algorithms": summary.get("algorithms"),
-            "Hashrate": summary.get("hashrate"),
-            "Hugepages": summary.get("hugepages")
-        }
-        return formatted_summary
+    def _get_headers(self):
+        headers = {}
+        if self._access_token:
+            headers['Authorization'] = f"Bearer {self._access_token}"
+        return headers
 
+    def fetch_summary(self):
+        headers = self._get_headers()
+        try:
+            response = requests.get(self._summary_url, headers=headers)
+            response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred while connecting to {self._summary_url}: {e}")
+            return None
 
     @property
     def hashrate(self):
         summary = self.fetch_summary()
-        if summary and "Hashrate" in summary:
-            self._hashrate = summary["Hashrate"]["total"][0]
+        if summary and "hashrate" in summary:
+            self._hashrate = summary["hashrate"]["total"][0]
         return self._hashrate
 
     @property
@@ -69,3 +57,10 @@ class XMRig:
         if summary and "connection" in summary:
             self._rejected_jobs = summary["connection"]["rejected"]
         return self._rejected_jobs
+
+    @property
+    def paused(self):
+        summary = self.fetch_summary()
+        if summary and "paused" in summary:
+            self._paused = summary["paused"]
+        return self._paused
