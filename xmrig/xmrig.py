fc552890c5aa5f4765d362fc91b2e7884cee6545
CoulterStutz <coultercash@gmail.com>
2024-06-26 15:40:04 -0600

    [XMRIG]: Return Summary and Hashrate

--- xmrig/xmrig.py
+++ xmrig/xmrig.py
@@ -3,7 +3,7 @@ class XMRig:
         self._ip = ip
         self._port = port
         self._access_token = access_token
-        
+
         self._base_url = f"http://{ip}:{port}/2"
         self._summary_url = f"{self._base_url}/summary"
         self._stats_url = f"{self._base_url}/stats"
@@ -16,3 +16,35 @@ class XMRig:
         self._connections_url = f"{self._base_url}/connections"
         self._threads_url = f"{self._base_url}/threads"
 
+    def _format_summary(self, summary):
+        formatted_summary = {
+            "ID": summary.get("id"),
+            "Worker ID": summary.get("worker_id"),
+            "Uptime (seconds)": summary.get("uptime"),
+            "Restricted": summary.get("restricted"),
+            "Memory": summary.get("resources", {}).get("memory", {}),
+            "Load Average": summary.get("resources", {}).get("load_average"),
+            "Hardware Concurrency": summary.get("resources", {}).get("hardware_concurrency"),
+            "Features": summary.get("features"),
+            "Results": summary.get("results"),
+            "Algorithm": summary.get("algo"),
+            "Connection": summary.get("connection"),
+            "Version": summary.get("version"),
+            "Kind": summary.get("kind"),
+            "User Agent": summary.get("ua"),
+            "CPU": summary.get("cpu"),
+            "Donate Level": summary.get("donate_level"),
+            "Paused": summary.get("paused"),
+            "Algorithms": summary.get("algorithms"),
+            "Hashrate": summary.get("hashrate"),
+            "Hugepages": summary.get("hugepages")
+        }
+        return formatted_summary
+
+
+    @property
+    def hashrate(self):
+        summary = self.fetch_summary()
+        if summary and "Hashrate" in summary:
+            self._hashrate = summary["Hashrate"]["total"][0]
+        return self._hashrate
