81fb36f4af8e7c4436d474579ac86aac69dd0209
CoulterStutz <coultercash@gmail.com>
2024-06-27 12:39:39 -0600

    [XMRIG]: Reflect Vers Change

--- xmrig/__init__.py
+++ xmrig/__init__.py
@@ -1,7 +1,7 @@
 __name__ = "xmrig"
 __author__ = "Coulter Stutz"
 __email__ = "coulterstutz@gmail.com"
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 from .xmrig import XMRig, XMRigAuthorizationError
 __all__ = ["XMRig", "XMRigAuthorizationError"]
