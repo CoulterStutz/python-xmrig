1c99be89b9cfdd39c7dc03137fb0240787fbd714
CoulterStutz <coultercash@gmail.com>
2024-06-30 19:49:46 -0600

    [XMRIG]: Reflect Version Change

--- xmrig/__init__.py
+++ xmrig/__init__.py
@@ -1,7 +1,7 @@
 __name__ = "xmrig"
 __author__ = "Coulter Stutz"
 __email__ = "coulterstutz@gmail.com"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 from .xmrig import XMRigAPI, XMRig, XMRigPool, PoolCoin, PoolAlgorithm
 __all__ = ["XMRig", "XMRigAPI", "XMRigPool", "PoolCoin", "PoolAlgorithm"]
